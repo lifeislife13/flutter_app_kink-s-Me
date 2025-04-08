8fe8864c8b5948eff3c85d0b8985b7ed1612b974
Life <carinewinnielopez@gmail.com>
2025-04-08 16:31:10 +0200

    Mise à jour Gradle & Firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.kinksme"
+        applicationId = "com.example.kinksme"get
         minSdk = 23
         targetSdk = 34
         versionCode = 1
